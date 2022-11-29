c87af3d4b2705a70d370fee4ad786a79a85e4ef7
ArpanGupta707 <arpangupta707@gmail.com>
2022-11-29 10:59:16 +0600

    modal db set

--- src/Pages/Home/Home/BookingModal/BookingModal.js
+++ src/Pages/Home/Home/BookingModal/BookingModal.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
 import { AuthContext } from '../../../../Context/AuthProvider';
 
 const BookingModal = ({ modproducts, setModProducts }) => {
@@ -26,8 +27,28 @@ const BookingModal = ({ modproducts, setModProducts }) => {
 
         }
 
-        console.log(booking)
-        setModProducts(null);
+        fetch('http://localhost:5000/buyProducts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.acknowledged) {
+                    setModProducts(null);
+                    toast.success('Product Confirmed')
+                }
+
+            })
+
+
+
+
+
     }
 
     return (
