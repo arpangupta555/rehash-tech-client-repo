e38e9ab33f84f1ce10bc17a9b202da15042e348e
ArpanGupta707 <arpangupta707@gmail.com>
2022-11-30 21:41:56 +0600

    dashboard3 set

--- src/Pages/Home/Home/Product/ProductCollection.js
+++ src/Pages/Home/Home/Product/ProductCollection.js
@@ -10,7 +10,7 @@ const ProductCollection = ({ pro, setModProducts }) => {
                     < img src={pro.picture} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
                     <div className="flex flex-col justify-between p-6 space-y-8">
                         <div className="space-y-2">
-                            <h2 className="text-3xl font-semibold tracking-wide">Items-name:{pro.product_name}</h2>
+                            <h2 className="text-xl ">Item name: <span className="text-2xl font-semibold tracking-wide">{pro.product_name}</span></h2>
                             <h5 className="text-xl">Sellers-name: <span className="text-2xl font-semibold tracking-wide">{pro.sellers_name}</span> </h5>
                             <h5 className="text-xl">Original Price: <span className="text-2xl font-semibold tracking-wide"> {pro.original_price}tk </span></h5>
                             <h5 className="text-xl">Resale Price: <span className="text-2xl font-semibold tracking-wide"> {pro.resale_price}tk </span></h5>
