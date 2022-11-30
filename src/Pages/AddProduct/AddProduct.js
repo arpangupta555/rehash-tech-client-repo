5d0d4b2f6be670ae056edb6fc9903e184b5289d6
ArpanGupta707 <arpangupta707@gmail.com>
2022-11-30 15:38:42 +0600

    addproduct entry set

--- src/Pages/AddProduct/AddProduct.js
+++ src/Pages/AddProduct/AddProduct.js
@@ -1,36 +1,138 @@
+import { data } from 'autoprefixer';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 
 const AddProduct = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const imageHostKey = process.env.REACT_APP_imgbb_key;
+
+    const handleAddProduct = data => {
+        const image = data.image[0];
+        const formData = new FormData();
+        formData.append('image', image);
+        const url = `https://api.imgbb.com/1/upload?expiration=600&key=${imageHostKey}`
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgData => {
+                if (imgData.success) {
+                    const product = {
+                        product_name: data.name,
+
+                        category_id: data.category,
+                        picture: imgData.data.url,
+
+                        sellers_name: data.sellerName,
+                        location: data.location,
+                        original_price: data.orprice,
+                        resale_price: data.reprice,
+                        years_of_use: data.year,
+                        time: data.time
+
+                    }
+
+                    fetch('http://localhost:5000/products', {
+                        method: 'POST',
+                        headers: {
+                            'content-type': 'application/json',
+                            authorization: `bearer ${localStorage.getItem('accessToken')}`
+                        },
+                        body: JSON.stringify(product)
+                    })
+                        .then(res => res.json())
+                        .then(result => {
+                            console.log(result);
+                            toast.success(`${data.name} is added successfully`);
+                            // navigate('/dashboard/managedoctors')
+                        })
+
+
+
+                }
+            })
+    }
     return (
-        <div>
-            <h2 className='text-xl text-center'>Sign Up</h2>
-            <form onSubmit={handleSubmit(handleSignUp)}>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label"> <span className="label-text">Name</span></label>
-                    <input type="text" {...register("name", {
-                        required: "Name is Required"
-                    })} className="input input-bordered w-full max-w-xs" />
-                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label"> <span className="label-text">Email</span></label>
-                    <input type="email" {...register("email", {
-                        required: true
-                    })} className="input input-bordered w-full max-w-xs" />
-                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label"> <span className="label-text">Password</span></label>
-                    <input type="password" {...register("password", {
-                        required: "Password is required",
-                        minLength: { value: 6, message: "Password must be 6 characters long" },
-                        pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must have uppercase, number and special characters' }
-                    })} className="input input-bordered w-full max-w-xs" />
-                    {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
-                </div>
-                <input className='btn btn-accent w-full mt-4' value="Sign Up" type="submit" />
-                {signUpError && <p className='text-red-600'>{signUpError}</p>}
-            </form>
+        <div >
+            <h2 className='text-3xl text-center'>Add Product</h2>
+            <div className='h-[900px] flex justify-center items-center'>
+                <form onSubmit={handleSubmit(handleAddProduct)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Name</span></label>
+                        <input type="text" {...register("name", {
+                            required: "Name is Required"
+                        })} className="input input-bordered w-full max-w-xs" />
+
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Seller Name</span></label>
+                        <input type="text" {...register("sellerName", {
+                            required: "Name is Required"
+                        })} className="input input-bordered w-full max-w-xs" />
+
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Location</span></label>
+                        <input type="text" {...register("location", {
+                            required: "Name is Required"
+                        })} className="input input-bordered w-full max-w-xs" />
+
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Original Price</span></label>
+                        <input type="text" {...register("orprice", {
+                            required: "Name is Required"
+                        })} className="input input-bordered w-full max-w-xs" />
+
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Resale Price</span></label>
+                        <input type="text" {...register("reprice", {
+                            required: "Name is Required"
+                        })} className="input input-bordered w-full max-w-xs" />
+
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Years of use</span></label>
+                        <input type="text" {...register("year", {
+                            required: "Name is Required"
+                        })} className="input input-bordered w-full max-w-xs" />
+
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Time</span></label>
+                        <input type="text" {...register("time", {
+                            required: "Name is Required"
+                        })} className="input input-bordered w-full max-w-xs" />
+
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Select Category</span></label>
+                        <select {...register("category", {
+                            required: true
+                        })} className="select select-bordered w-full max-w-xs">
+
+                            <option value={'6382d7367a7542d260ff1baf'}>Iphone</option>
+                            <option value={'6382d7367a7542d260ff1bb0'}>Samsung</option>
+                            <option value={'6382d7367a7542d260ff1bb1'}>Xiaomi</option>
+                        </select>
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Photo</span></label>
+                        <input type="file" {...register("image", {
+                            required: "Photo is Required"
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.img && <p className='text-red-500'>{errors.img.message}</p>}
+                    </div>
+
+                    <input className='btn btn-accent  mt-4' value="Submit" type="submit" />
+                    {/* {signUpError && <p className='text-red-600'>{signUpError}</p>} */}
+                </form>
+            </div>
         </div>
     );
 };
